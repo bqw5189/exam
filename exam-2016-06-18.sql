83e1b11ee3e3f7f3546c85d750ddcb16e393b7b2
tom <tom@daoshenggroup.com>
2016-06-19 22:46:22 +0800

    addd create table t_te_answer(

--- exam-2016-06-18.sql
+++ exam-2016-06-18.sql
@@ -84,7 +84,7 @@ CREATE TABLE `ss_user` (
   `class_id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `login_name` (`login_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -93,8 +93,36 @@ CREATE TABLE `ss_user` (
 
 LOCK TABLES `ss_user` WRITE;
 /*!40000 ALTER TABLE `ss_user` DISABLE KEYS */;
-INSERT INTO `ss_user` VALUES (1,'admin','管理员','691b14d79bf0fa2215f155235df5e670b64394cc','7efbd59d9741d34f','admin','2016-06-19 12:33:47',1),(2,'user','学生','2488aa0c31c624687bd9928e0a5d29e7d1ed520b','6d65d24122c30500','user','2016-06-19 12:33:48',1),(3,'teacher','老师','3cb3228620732f1e570fe29920f3aa0df636ad9d','ccdef9df1622648f','teacher','2016-06-19 12:33:48',1);
+INSERT INTO `ss_user` VALUES (1,'admin','管理员','691b14d79bf0fa2215f155235df5e670b64394cc','7efbd59d9741d34f','admin','2016-06-19 12:33:47',1),(2,'user','学生','2488aa0c31c624687bd9928e0a5d29e7d1ed520b','6d65d24122c30500','user','2016-06-19 12:33:48',1),(3,'teacher','老师','3cb3228620732f1e570fe29920f3aa0df636ad9d','ccdef9df1622648f','teacher','2016-06-19 12:33:48',1),(4,'user1','user1','d5f510402a9148de7d3c4b6d1c98082b19f75d0c','e70d793173712bd4','student','2016-06-19 13:10:26',2);
 /*!40000 ALTER TABLE `ss_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_te_answer`
+--
+
+DROP TABLE IF EXISTS `t_te_answer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_te_answer` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) DEFAULT NULL,
+  `title` varchar(512) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `course` varchar(256) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `c_type` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `answer` text COLLATE utf8_unicode_ci,
+  `answer_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_te_answer`
+--
+
+LOCK TABLES `t_te_answer` WRITE;
+/*!40000 ALTER TABLE `t_te_answer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_te_answer` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -179,7 +207,7 @@ CREATE TABLE `t_te_exam_question` (
 
 LOCK TABLES `t_te_exam_question` WRITE;
 /*!40000 ALTER TABLE `t_te_exam_question` DISABLE KEYS */;
-INSERT INTO `t_te_exam_question` VALUES (1,1,'某化合物分子离子峰区的相对峰度为M:(M+2):(M+4)=9:6:1，则分子式中含()个氯原子。','1',5,201,1),(2,1,'在化合物CH3Br的质谱中，M和M+2峰的相对强度比应为：','8',5,201,1),(3,1,'某一含卤素的化合物，其质谱图上同位素峰强比M:M+2:M+4=3:4:1，则该化合物中含有（）','11',5,201,1),(4,1,'本项目的采样地点是','13',5,201,1),(5,1,'本项目的检验指标是','18',5,201,1),(6,1,'本项目的检验标准是','23',5,201,1),(7,1,'本项目的样品名称是','27',5,201,1),(8,1,'本项目所用方法为','30',5,201,1),(9,1,'在气相色谱定量分析中，只有试样中所有组分都能出彼此分离较好的峰才能使用的方法是(    )','31',5,201,1),(10,2,'本项目检测样品任务单上有以下哪些内容','35,36,37,38,39,40',10,201,1);
+INSERT INTO `t_te_exam_question` VALUES (1,1,'某化合物分子离子峰区的相对峰度为M:(M+2):(M+4)=9:6:1，则分子式中含()个氯原子。','1',5,201,1),(2,1,'在化合物CH3Br的质谱中，M和M+2峰的相对强度比应为：','8',5,201,1),(3,1,'某一含卤素的化合物，其质谱图上同位素峰强比M:M+2:M+4=3:4:1，则该化合物中含有（）','11',5,201,1),(4,1,'本项目的采样地点是','13',5,201,1),(5,1,'本项目的检验指标是','18',5,201,1),(6,1,'本项目的检验标准是','23',5,201,1),(7,1,'本项目的样品名称是','27',5,201,1),(8,1,'本项目所用方法为','30',5,201,1),(9,1,'在气相色谱定量分析中，只有试样中所有组分都能出彼此分离较好的峰才能使用的方法是(    )','34',5,201,1),(10,2,'本项目检测样品任务单上有以下哪些内容',NULL,10,201,1);
 /*!40000 ALTER TABLE `t_te_exam_question` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -266,7 +294,7 @@ CREATE TABLE `t_te_project` (
 
 LOCK TABLES `t_te_project` WRITE;
 /*!40000 ALTER TABLE `t_te_project` DISABLE KEYS */;
-INSERT INTO `t_te_project` VALUES (201,100,'第二单元',''),(202,100,'第三单元',''),(203,101,'第一单元',''),(204,102,'第一单元',''),(205,102,'第一单元',''),(206,104,'第一单元',''),(207,104,'第二单元','');
+INSERT INTO `t_te_project` VALUES (200,100,'第二单元',''),(201,100,'第三单元',''),(202,101,'第一单元',''),(203,102,'第一单元',''),(204,102,'第一单元',''),(205,104,'第一单元',''),(206,104,'第二单元','');
 /*!40000 ALTER TABLE `t_te_project` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -317,7 +345,7 @@ CREATE TABLE `t_te_select_items` (
 
 LOCK TABLES `t_te_select_items` WRITE;
 /*!40000 ALTER TABLE `t_te_select_items` DISABLE KEYS */;
-INSERT INTO `t_te_select_items` VALUES (1,1,'1',1),(2,1,'2',0),(3,1,'3',0),(4,1,'4',0),(5,2,'98.9:1.1',0),(6,2,'98.9:0.02',0),(7,2,'98.9:0.02',0),(8,2,'1:01',1),(9,3,'两个氯',0),(10,3,'两个溴',0),(11,3,'一个氯和一个溴',1),(12,3,'两个氯和两个溴',0),(13,4,'北校区',1),(14,4,'朝阳垡头地区',0),(15,4,'南校区',0),(16,5,'BOD',0),(17,5,'COD',0),(18,5,'PAH',1),(19,5,'TP',0),(20,6,'GB 11914-1989',0),(21,6,'GB 3838-2002',0),(22,6,'GB 11893-1989',0),(23,6,'HJ 478-2009',1),(24,7,'污水中TP含量测定',0),(25,7,'污水中BOD含量测定',0),(26,7,'污水中DO含量测定',0),(27,7,'北校区大气',1),(28,8,'气相色谱',0),(29,8,'液相色谱',0),(30,8,'液相色谱',1),(31,9,'归一化方法',1),(32,9,'内标法',0),(33,9,'外标法—比较法',0),(34,9,'外标法—标准工作曲线法',0),(35,10,'任务单编号',1),(36,10,'检测样品的名称',1),(37,10,'检验依据',1),(38,10,'检验标准',1),(39,10,'检验标准',1),(40,10,'任务完成日期',1);
+INSERT INTO `t_te_select_items` VALUES (1,1,'1',1),(2,1,'2',0),(3,1,'3',0),(4,1,'4',0),(5,2,'98.9:1.1',0),(6,2,'98.9:0.02',0),(7,2,'98.9:0.02',0),(8,2,'1:01',1),(9,3,'两个氯',0),(10,3,'两个溴',0),(11,3,'一个氯和一个溴',1),(12,3,'两个氯和两个溴',0),(13,4,'北校区',1),(14,4,'朝阳垡头地区',0),(15,4,'南校区',0),(16,5,'BOD',0),(17,5,'COD',0),(18,5,'PAH',1),(19,5,'TP',0),(20,6,'GB 11914-1989',0),(21,6,'GB 3838-2002',0),(22,6,'GB 11893-1989',0),(23,6,'HJ 478-2009',1),(24,7,'污水中TP含量测定',0),(25,7,'污水中BOD含量测定',0),(26,7,'污水中DO含量测定',0),(27,7,'北校区大气',1),(28,8,'气相色谱',0),(29,8,'液相色谱',0),(30,8,'液相色谱',1),(32,9,'内标法',0),(33,9,'外标法—比较法',0),(34,9,'外标法—标准工作曲线法',1);
 /*!40000 ALTER TABLE `t_te_select_items` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -356,4 +384,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-06-19 21:06:37
+-- Dump completed on 2016-06-19 22:45:51
--- src/main/webapp/WEB-INF/views/makexam/makexamlist.jsp
+++ src/main/webapp/WEB-INF/views/makexam/makexamlist.jsp
@@ -57,12 +57,11 @@
             <thead>
             <tr>
                 <th width="5%">序号</th>
-                <th width="30%">试卷名称</th>
-                <th width="30%">试卷说明</th>
+                <th width="20%">试卷名称</th>
+                <th width="20%">试卷说明</th>
                 <th width="5%">答题时间(分钟)</th>
-                <th width="10%">创建时间</th>
                 <th width="5%">状态</th>
-                <th width="15%">操作</th>
+                <th width="20%">操作</th>
             </tr>
             </thead>
             <tbody>
@@ -73,7 +72,7 @@
                     <td>${ exam.examName }</td>
                     <td>${ exam.examRemark}</td>
                     <td>${ exam.examTime}</td>
-                    <td>${ exam.examBeginDate}</td>
+                    <%--<td>${ exam.examBeginDate}</td>--%>
                     <td>
                         <c:if test="${exam.state==1 }">开始考试</c:if>
                         <c:if test="${exam.state==0 }">未考试</c:if>
